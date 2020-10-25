47178a830353e84ab7abb3073f2c827036f3889a
Marco Rougeth <marco@rougeth.com>
2020-10-25 20:07:55 +0000

    Add logs

--- pybrbot/discord/__init__.py
+++ pybrbot/discord/__init__.py
@@ -1,18 +1,19 @@
-# bot.py
+import logging
 import os
 from collections import defaultdict
 
 import discord
+from loguru import logger
 from discord.ext import commands
 from . import config, messages, utils
 
-
 intents = discord.Intents(members=True, guilds=True)
-bot = commands.Bot(command_prefix="/")
+bot = commands.Bot(command_prefix="/", intents=intents)
 invite_tracker = utils.InviteTracker(bot)
 
 
 async def welcome_message(member):
+    logger.info("Sending welcome message. member=%s", member.display_name)
     role = utils.get_role(member.guild, "codigo-de-conduta")
     await member.create_dm()
     await member.dm_channel.send(messages.WELCOME.format(
@@ -23,19 +24,19 @@ async def welcome_message(member):
 @bot.event
 async def on_ready():
     await invite_tracker.sync()
-    print(invite_tracker.diff())
     
-    print(f'{bot.user} has connected to Discord!')
+    logger.info(f'{bot.user} has connected to Discord!\n')
     for guild in bot.guilds:
         print(guild.name, guild.id)
         for role in guild.roles:
-            print(role.name, role.id)
+            print(f"  {role.name}: {role.id}")
         
         print()
 
 
 @bot.event
 async def on_member_join(member):
+    logger.info("New member joined. member=%r", member.display_name)
     await invite_tracker.check_member_role(member)
     await welcome_message(member)
 
--- pybrbot/discord/utils.py
+++ pybrbot/discord/utils.py
@@ -1,4 +1,7 @@
+import logging
+
 from . import config
+from loguru import logger
 
 import discord
 
@@ -19,6 +22,7 @@ class InviteTracker:
         return {invite.code: invite.uses for invite in invites}
 
     async def sync(self):
+        logger.info("Syncing invite codes usage")
         invites = await self.get_invites()
         self.old_invites = self.invites
         self.invites = invites
@@ -29,19 +33,24 @@ class InviteTracker:
             old_uses = self.old_invites.get(code, 0)
             if uses > old_uses:
                 diff[code] = uses - old_uses
+                logger.info("Invite code used. code=%r, uses=%r", code, uses)
 
         return diff
 
     async def check_member_role(self, member):
+        logger.info("Checking member role. member=%r", member.display_name)
         await invite_tracker.sync()
         invite_diff = invite_tracker.diff()
 
         for code, uses in invite_diff.items():
+            logger.info("Invite code used. code=%r", code)
             new_role = discord.Object(INVITES_ROLES.get(code))
             if new_role and uses == 1:
+                logger.info("Updating member role. member=%r, role=%r", member.display_name, new_role)
                 await member.add_roles(new_role)
             elif uses > 1:
-                print("Two or more users joined server between invite tracker updates.")
+                logger.warning("Two or more users joined server between invite tracker updates.")
+
 
 
 def get_role(guild, role_name):
