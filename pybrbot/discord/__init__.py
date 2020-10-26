8b0d34ca0413a7ccab318bf6d8ae9f09a8ce024a
Marco Rougeth <marco@rougeth.com>
2020-10-26 16:37:51 +0000

    Fix commands

--- pybrbot/discord/__init__.py
+++ pybrbot/discord/__init__.py
@@ -1,25 +1,22 @@
 import logging
 import os
 from collections import defaultdict
+from typing import Optional
 
 import discord
 from loguru import logger
 from discord.ext import commands
 from . import config, messages, utils
 
-intents = discord.Intents(members=True, guilds=True)
-bot = commands.Bot(command_prefix="/", intents=intents)
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 invite_tracker = utils.InviteTracker(bot)
 
 
-async def welcome_message(member):
+async def send_dm_member(member, message):
     logger.info(f"Sending welcome message. member={member.display_name}")
-    role = utils.get_role(member.guild, "codigo-de-conduta")
     await member.create_dm()
-    await member.dm_channel.send(messages.WELCOME.format(
-        name=member.name,
-        cdc_team=role,
-    ))
+    await member.dm_channel.send(message)
 
 @bot.event
 async def on_ready():
@@ -37,19 +34,67 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     logger.info(f"New member joined. member={member.display_name}")
+    
+    role = utils.get_role(member.guild, "codigo-de-conduta")
+    message = messages.WELCOME.format(
+        name=member.name,
+        cdc_team=role,
+    )
     await invite_tracker.check_member_role(member)
-    await welcome_message(member)
+    await send_dm_member(member, message)
 
 
 @bot.command()
-async def welcome(ctx, *args):
-    logger.info(f"/welcome command trigger by member. member={ctx.member.author}")
-    await welcome_message(ctx.message.author)
+async def welcome(ctx):
+    member = ctx.message.author
+    logger.info(f"!welcome command trigger by member. member={member}, type={type}")
+
+    cdc_team = utils.get_role(ctx.guild, "codigo-de-conduta")
+    message = messages.WELCOME.format(
+        name=member.name,
+        cdc_team=cdc_team,
+    )
+    await send_dm_member(member, message)
+
+    channels = ctx.guild.channels
+    channel_speakers = discord.utils.get(channels, name="palestrantes").mention
+    channel_announcements = discord.utils.get(channels, name="anuncios").mention
+    channel_pep0 = discord.utils.get(channels, name="trilha-pep0").mention
+    channel_pep8 = discord.utils.get(channels, name="trilha-pep8").mention
+    channel_pep20 = discord.utils.get(channels, name="trilha-pep20").mention
+    channel_pep404 = discord.utils.get(channels, name="trilha-pep404").mention
+
+    message = messages.WELCOME_SPEAKER.format(
+        name=member.name,
+        channel_speakers=channel_speakers,
+        channel_announcements=channel_announcements,
+        channel_pep0=channel_pep0,
+        channel_pep8=channel_pep8,
+        channel_pep20=channel_pep20,
+        channel_pep404=channel_pep404,
+    )
+    await send_dm_member(member, message)
+        
+
+
+
+
+@bot.command()
+async def channel(ctx, *args):
+    logger.info(f"!channel command trigger by member. member={ctx.message.author}")
+
+    message = f"Guild: {ctx.message.guild}\n"
+    message += "Channels:\n"
+    for channel in ctx.message.guild.channels:
+        message += f"- {channel.name} {channel.mention}"
+
+    await ctx.message.author.create_dm()
+    await ctx.message.author.dm_channel.send(message)
 
 
 @bot.command()
 async def cdc(ctx, *args):
-    logger.info(f"/cdc command trigger by member. member={ctx.member.author}")
+    logger.info(f"/cdc command trigger by member. member={ctx.message.author}")
     role = utils.get_role(ctx.guild, "codigo-de-conduta")
     message = f"{role.mention} - https://python.org.br/cdc"
     await ctx.channel.send(messages.CDC.format(
--- pybrbot/discord/messages.py
+++ pybrbot/discord/messages.py
@@ -1,10 +1,32 @@
 WELCOME = """
-Olá {name}!
+Olá **{name}**!
 
 Grade de palestras e tutoriais: https://2020.pythonbrasil.org.br/grade
 
 Código de Conduta: https://python.org.br/cdc
 Time de resposta do CdC: {cdc_team}
+"""
+
+WELCOME_SPEAKER = """
+Olá **{name}**! 🥳 🎉
+
+Obrigada por participar na **Python Brasil 2020!** 
+A sua palestra é muito importante para a nossa comunidade!
+
+Aqui vão algumas informações relevantes:
+- O canal {channel_speakers} é para uso exclusivo dos palestrantes. A organização estará presente para ajudar no que for preciso.
+- O canal {channel_announcements} será usado pela organização para anúncios importantes, como por exemplo, mudança de horários.
+- Cada trilha de palestras tem seu próprio canal:
+  - {channel_pep0}
+  - {channel_pep8}
+  - {channel_pep20}
+  - {channel_pep404}
+- A grade de palestras e tutoriais completa está online em: https://2020.pythonbrasil.org.br/grade
+
+**Importante**:
+- O Código de Conduta pode ser lido em: https://python.org.br/cdc
+- Time de resposta do Código de Conduta é o **@codigo-de-conduta**.
+
 """
 
 CDC = """
