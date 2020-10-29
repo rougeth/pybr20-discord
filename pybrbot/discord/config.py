62a97fcfe617fb8cf5d5610533b65c5550cb7b78
Marco Rougeth <marco@rougeth.com>
2020-10-29 19:38:35 +0000

    Add prod.toml and dev.toml config files

--- pybrbot/discord/config.py
+++ pybrbot/discord/config.py
@@ -1,4 +1,9 @@
+import toml
 from decouple import config
 
 TOKEN = config('DISCORD_TOKEN')
 GUILD = config("DISCORD_GUILD_ID")
+CONFIG_FILE = config("CONFIG_FILE", "config/dev.toml")
+
+local_config = toml.load(CONFIG_FILE)
+locals().update(local_config)
